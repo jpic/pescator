3d7236fcd8d5f77e7ce0bdb3b8f14cba0e436df0
jpic <jamespic@gmail.com>
2013-11-17 18:25:07 +0100

    Added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This is how I avoid the need of fabric: with a 17 SLOC bash function.
+
+# Runs a local function or remote command on one or several servers through ssh
+# in given paths.
+# For example, source this module and try:
+##  # with a local function
+##  function hello() { echo "Hello from `pwd`"; }
+##  remote hello somehost: otherhost:/path/to/test
+##  # with a remote command
+##  remote "git pull origin master" somehost:/path
+function remote() {
+    local do="$1"
+    shift
+    local targets="$*"
+
+    local target=""
+    local target_host=""
+    local target_path=""
+
+    for target in $targets; do
+        target_host="${target%%:*}"
+        target_path="${target/$target_host:/}"
+
+        if [[ -n "$(declare -f $do)" ]]; then
+            echo "Running $do() on $target_host in $target_path"
+            ssh $target_host "cd $target_path && $(declare -f $do) && $do"
+        else
+            echo "Running $do on $target_host in $target_path"
+            ssh $target_host "cd $target_path && $do"
+        fi
+    done
+}
+
+git push origin master
+
+function deploy() {
+    source ../env/bin/activate
+    git stash
+    git pull origin master
+    ./manage.py collectstatic -l --noinput
+    touch pescator/wsgi.py
+}
+
+remote deploy pescator@tina:pescator/
