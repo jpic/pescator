08e103b192c41bd6025b35c2d9b2a4dc770d19d0
jpic <jamespic@gmail.com>
2013-11-17 16:48:15 +0100

    Internationalized some url patterns

--- pescator/urls.py
+++ pescator/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -15,7 +16,13 @@ urlpatterns = patterns('',
     url(r'^search/', include('haystack.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^redactor/', include('redactor.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += i18n_patterns('',
     url(r'^guestbook/', include('guestbook.urls')),
     url(r'^', include('pages.urls')),
+)
+
+urlpatterns += patterns('',
     url(r'^$', views.HomeView.as_view(), name='home'),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
