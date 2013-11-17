2a4eb8a3caeadc91d2d2bd458cea89440b108878
jpic <jamespic@gmail.com>
2013-11-17 18:33:09 +0100

    Hard code fr as default language

--- pescator/views.py
+++ pescator/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.utils import translation
 from django.views import generic
 
 from pages.models import Page
@@ -5,6 +7,9 @@ from pages.models import Page
 
 class HomeView(generic.RedirectView):
     def get_redirect_url(self, **kwargs):
+        if self.request.path == '/':
+            translation.activate(settings.LANGUAGE_CODE)
+
         try:
             return Page.objects.all().order_by('pk')[0].get_absolute_url()
         except IndexError:
