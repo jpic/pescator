771d3a3a3127fe100e156735220bd332f9a0e23e
jpic <jamespic@gmail.com>
2013-11-17 16:32:32 +0100

    Redirect to admin if there is no page

--- pescator/views.py
+++ pescator/views.py
@@ -5,4 +5,7 @@ from pages.models import Page
 
 class HomeView(generic.RedirectView):
     def get_redirect_url(self, **kwargs):
-        return Page.objects.all().order_by('pk')[0].get_absolute_url()
+        try:
+            return Page.objects.all().order_by('pk')[0].get_absolute_url()
+        except IndexError:
+            return '/admin/'
